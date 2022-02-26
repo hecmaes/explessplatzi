cb5202657c6f3de9c4831ff7f993cb03d351fa9b
hecmaes@gmail.com <hecmaes@gmail.com>
2022-02-25 20:32:07 -0500

    commit desarrollo 2

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('no permitido'));
+        }
     }
-  }
 }
 app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
+    res.send('Hola mi server en express ssssssssssssssssssss sssssssssssssss');
 });
 
 app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
+    res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
@@ -37,5 +37,5 @@ app.use(errorHandler);
 
 
 app.listen(port, () => {
-  console.log('Mi port' +  port);
+    console.log('Mi port' + port);
 });
